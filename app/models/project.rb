e63a0f96d3a7a5fd94dbe8e44fcbb345421ae060
J. Nathan Matias <natematias@gmail.com>
2012-12-27 08:34:51 -0500

    updated header to be a bootstrap well. Added ability to add a host to a project, with test

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,20 +5,23 @@ class Project < ActiveRecord::Base
   has_many :hosts, :through => :project_hosts, :order=>"project_hosts.order ASC"
 
   def add_person_to_project(person, label, order=nil)
-    if self.people.size == 0
-      max_order = 0
-    else
-      max_order = self.project_people.collect{|p| p.order}.max
-    end
-    
-    if !order.nil? and order <= max_order
-      return nil
-    end
-
+    max_order = self.project_people.collect{|p| p.order}.max
+    max_order = 0 if max_order.nil?
+    return nil if !order.nil? and order <= max_order
     order = max_order + 1 if order.nil?
     
     ProjectPerson.create!({:project=>self, :person=>person, 
                            :label=>label, :order=>order})
+  end
+
+  def add_host_to_project(host, label, order=nil)
+    max_order = self.project_hosts.collect{|h|h.order}.max
+    max_order = 0 if max_order.nil?
+    return nil if !order.nil? and order <= max_order
+    order = max_order + 1 if order.nil?
+
+    ProjectHost.create!({:project=>self, :host=>host,
+                         :label=>label, :order=>order})
   end
   
 end
--- test/unit/project_test.rb
+++ test/unit/project_test.rb
@@ -19,4 +19,23 @@ class ProjectTest < ActiveSupport::TestCase
     assert_equal 3, projects(:one).project_people.size
     assert_equal 3, projects(:one).project_people.last.order
   end
+
+  test "add host to project" do
+    assert_equal 2, projects(:one).hosts.size
+    medialab = Host.create!({:name=>"MIT Media Lab", :twitter_account=>"medialab",
+                            :uri=>"https://twitter.com/medialab"})
+    #this should fail, since something else has order one
+    projects(:one).add_host_to_project(medialab, "supporter", 1)
+
+    projects(:one).reload
+    assert_equal 2, projects(:one).hosts.size
+    assert_equal 2, projects(:one).project_hosts.size
+
+    #this should succeed and assign the order as 3
+    projects(:one).add_host_to_project(medialab, "contributor")
+    projects(:one).reload
+    assert_equal 3, projects(:one).hosts.size
+    assert_equal 3, projects(:one).project_hosts.size
+    assert_equal 3, projects(:one).project_hosts.last.order
+  end
 end
