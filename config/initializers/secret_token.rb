6cc8b83b34fa4d13dca02cb5319c9e2447185786
Frederick Cheung <frederick.cheung@gmail.com>
2016-09-11 18:48:14 +0100

    replace secret_token with secrets.yml

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Acknowledger::Application.config.secret_token = '1c0509951241356b7f0e4833a130a219feebc6a3dee0daed38fb077e46b09b0ce761ca503c764e05da5685a4cfb6cf4ff32d6fad0b923dcc50e9e37b0022dbab'
