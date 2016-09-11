41ab1fee26bec104eef76fe8d4a3665e7f133df9
Frederick Cheung <frederick.cheung@gmail.com>
2016-09-11 18:48:50 +0100

    use json for cookie serialization

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
+Rails.application.config.action_dispatch.cookies_serializer = :json
+
 Acknowledger::Application.config.session_store :cookie_store, key: '_acknowledger_session'
 
 # Use the database for sessions instead of the cookie-based default,
