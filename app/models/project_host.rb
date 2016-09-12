6cb672cf9a48f464d3b8949fc3b2576fa0e27237
Frederick Cheung <frederick.cheung@gmail.com>
2016-09-12 07:08:06 +0100

    hide project assoc when editing project_host/project_person

--- app/models/project_host.rb
+++ app/models/project_host.rb
@@ -4,6 +4,9 @@ class ProjectHost < ActiveRecord::Base
 
   rails_admin do
     object_label_method :admin_description
+    configure :project do
+      hide
+    end   
   end
 
   def admin_description
--- app/models/project_person.rb
+++ app/models/project_person.rb
@@ -1,9 +1,12 @@
 class ProjectPerson < ActiveRecord::Base
   belongs_to :person
-  belongs_to :project
+  belongs_to :project, inverse_of: :project_people
 
   rails_admin do
     object_label_method :admin_description
+    configure :project do
+      hide
+    end    
   end
 
   def admin_description
